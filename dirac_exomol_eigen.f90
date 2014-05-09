ca1892704e87d7092b9fdb3d61e60f5460f56629
Filippo Spiga <spiga.filippo@gmail.com>
2014-05-09 09:28:24 +0100

    ELPA is capable to print internal timing by itself if     elpa_print_times = .true.

--- dirac_exomol_eigen.f90
+++ dirac_exomol_eigen.f90
@@ -536,6 +536,10 @@ program dirac_exomol_eigen
     ! initialize the eigensolver  !
     ! --------------------------- !
     !
+#if defined(__ELPA) && defined (__ELPA_TIMING)
+    elpa_print_times = .true.
+#endif
+    !
     select case (eigensolver)
           !
         case (1)
@@ -682,26 +686,8 @@ program dirac_exomol_eigen
         write(out,'(/a,f12.6,a)') 'Time to diagonalize matrix is ',t2-t1,'sec'
         if (info .eq. 0) then
             write(out,"(/'Diagonalization finished successfully!')")
-
-#if defined(__ELPA)
-            if (eigensolver .ge. 3) then
-            write(out,'(/a)') 'Detailed internal ELPA timing :'
-                write(out,'(a,f9.2)')         '  Time tridiag_real    : ',time_evp_fwd
-                if (eigensolver .eq. 4) then
-                    write(out,'(a,f9.2)') '  > trans_band_real    : ',trans_band_real
-                endif
-                write(out,'(a,f9.2)')         '  Time solve_tridi     : ',time_evp_solve
-                write(out,'(a,f9.2)')         '  Time trans_ev_real   : ',time_evp_back
-                if (eigensolver .eq. 4) then
-                    write(out,'(a,f9.2)') '  > trans_full_real    : ',trans_full_real
-                endif
-            endif
-#endif
-
-        else if (info .lt. 0) then
-            write(out,"(/'Info is less than zero. Info is equal to ', i8)") info
         else
-            write(out,"(/'Info is larger than zero. Info is equal to ', i8)") info
+            write(out,"(/'Something went bananas... info = ', i6)") info
         endif
     endif
     !
